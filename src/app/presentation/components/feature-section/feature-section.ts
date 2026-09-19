3593981302638b7ee842e5e918445eefc23be7ea
malumunayco <malumunaycoa05@gmail.com>
2026-09-19 00:18:25 -0500

    feat(components): add feature section component

---
+++ src/app/presentation/components/feature-section/feature-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-feature-section',
+  styleUrl: './feature-section.css',
+  templateUrl: './feature-section.html',
+})
+export class FeatureSection {}
